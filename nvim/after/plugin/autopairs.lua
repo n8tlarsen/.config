d54f72256ec787c34bde76be9c4789e0a4e6478a
Nathan <n8tlarsen@gmail.com>
2022-09-24 13:04:27 -0500

    Add autopairs fast_wrap

--- nvim/after/plugin/autopairs.lua
+++ nvim/after/plugin/autopairs.lua
@@ -6,8 +6,18 @@ npairs.setup({
     ts_config = {
         lua = {'string'},-- it will not add a pair on that treesitter node
         javascript = {'template_string'},
-        java = false,-- don't check treesitter on java
-    }
+    },
+    enable_check_bracket_line = false,
+    fast_wrap = {
+      map = '<M-e>',
+      chars = { '{', '[', '(', '"', "'" },
+      pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+      end_key = '$',
+      keys = 'qwertyuiopzxcvbnmasdfghjkl',
+      check_comma = true,
+      highlight = 'Search',
+      highlight_grey='Comment'
+    },
 })
 
 local ts_conds = require('nvim-autopairs.ts-conds')
