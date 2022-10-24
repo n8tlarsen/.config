f08f77b223749ac99ae1505c27034beab98871a7
n8tlarsen <n8tlarsen@gmail.com>
2022-10-24 16:32:52 -0500

    Add esc terminal keybind

--- nvim/after/plugin/keymap.lua
+++ nvim/after/plugin/keymap.lua
@@ -1,4 +1,5 @@
 local nnoremap = require("user.keymap").nnoremap
+local tnoremap = require("user.keymap").tnoremap
 
 nnoremap("<leader>e", "<cmd>Ex<CR>")
 nnoremap("<leader>a", function() require("harpoon.mark").add_file() end, silent)
@@ -7,3 +8,4 @@ nnoremap("<C-j>", function() require("harpoon.ui").nav_file(1) end, silent)
 nnoremap("<C-k>", function() require("harpoon.ui").nav_file(2) end, silent)
 nnoremap("<C-l>", function() require("harpoon.ui").nav_file(3) end, silent)
 nnoremap("<C-;>", function() require("harpoon.ui").nav_file(4) end, silent)
+tnoremap("<Esc>", "<C-\\><C-n>")
