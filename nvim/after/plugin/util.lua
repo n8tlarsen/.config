a43009c7bd40ff12ed1392ab1554c65e66ee5aca
n8tlarsen <n8tlarsen@gmail.com>
2023-03-08 15:37:43 -0600

    Configure lsp floating window

--- nvim/after/plugin/util.lua
+++ nvim/after/plugin/util.lua
@@ -1,2 +1,12 @@
 vim.cmd("syntax enable")
 vim.cmd("filetype plugin indent on")
+vim.diagnostic.config({
+    virtual_text = {
+        severity = vim.diagnostic.severity.INFO,
+    },
+    signs = {
+        priority = 100
+    },
+    severity_sort = true
+})
+vim.keymap.set("n", "<leader>y", function() vim.diagnostic.open_float({scope = "line"}) end)
