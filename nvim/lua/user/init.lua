d84174e8ba973a833e5e44fe40ba86f33a72e9dd
n8tlarsen <n8tlarsen@gmail.com>
2025-06-06 14:29:31 -0500

    Add wsl-open note

--- nvim/lua/user/init.lua
+++ nvim/lua/user/init.lua
@@ -60,6 +60,8 @@ require("lazy").setup({
         priority = math.huge
     },
     {
+        -- Requires wsl-open on WSL2
+        -- https://github.com/4U6U57/wsl-open
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         ft = { "markdown" },
